c1dfbe5241f981beb35b8f0a98ba07b8c35a05b6
Madhur Kapoor <madhur.kapoor@gmail.com>
2022-01-09 13:24:03 +0530

    Added Models and Migrations.

---
+++ ECommerceApp/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using ECommerceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(x => new
+            {
+                x.ActorId,
+                x.MovieId
+            });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(x => x.Movie).WithMany(x => x.Actors_Movies).HasForeignKey(x => x.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(x => x.Actor).WithMany(x => x.Actor_Movies).HasForeignKey(x => x.ActorId);
+
+            base.OnModelCreating(modelBuilder);
+
+            
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
